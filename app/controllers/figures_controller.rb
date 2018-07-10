ea0c9f32caccddec7b7bb15d6b4c9a5df357f880
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-10 19:11:12 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -41,6 +41,21 @@ class FiguresController < ApplicationController
 
   post '/figures/:id' do
     @figure = Figure.find_by_id(params["id"])
+    if !params["title"]["name"].empty?
+      @title = Title.create(name: params["title"]["name"])
+      @figure.titles << @title
+    end
+    @title_ids = params["figure"]["title_ids"]
+    @title_ids.each {|id| @figure.titles << Title.find_by_id(id)} unless @title_ids.nil?
+    if !params["landmark"]["name"].empty?
+      @landmark = Landmark.create(name: params["landmark"]["name"])
+      @landmark.year_completed = params["landmark"]["year_completed"] unless nil?
+      @landmark.save
+      @figure.landmarks << @landmark
+    end
+    @landmark_ids = params["figure"]["landmark_ids"]
+    @landmark_ids.each {|id| @figure.landmarks << Landmark.find_by_id(id)} unless @landmark_ids.nil?
+    @figure.save
     binding.pry
   end
 
