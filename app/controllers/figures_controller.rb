591209a90240cfe78cc32e3b17c417729abc92d8
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-10 17:54:15 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,6 +5,10 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    @figure = Figure.create(name: params["figure"]["name"])
+    if !params["title"]["name"].empty?
+      @title = Title.find_or_create_by(name: params["title"]["name"])
+    end
     binding.pry
   end
 
