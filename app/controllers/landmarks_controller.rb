a662590f3bff6b80e33d4077f72e9c5dd78207a2
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-10 19:23:53 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -8,7 +8,7 @@ class LandmarksController < ApplicationController
       erb :'/landmarks/new'
   end
 
-  post '/landmarks' do 
+  post '/landmarks' do
     binding.pry
     @landmark = Landmark.create(name: params["name"], year_completed: params["year_completed"])
     "Successfully created a New Landmark"
