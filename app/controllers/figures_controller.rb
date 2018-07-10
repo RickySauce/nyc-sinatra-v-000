003dd7730e4b6629a1dfb2dd5e53f312b7a5557d
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-10 18:23:35 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,8 +14,9 @@ class FiguresController < ApplicationController
     @title_ids.each {|id| @figure.titles << Title.find_by_id(id)} unless @title_ids.nil?
     if !params["landmark"]["name"].empty?
       @landmark = Landmark.create(name: params["landmark"]["name"])
-      binding.pry
       @landmark.year_completed = params["landmark"]["year_completed"] unless nil?
+      @landmark.save
+      binding.pry
       @figure.landmarks << @landmark
     end
     @landmark_ids = params["figure"]["landmark_ids"]
